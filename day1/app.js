e1d4a742986367a7c790d0dddb90c5f019872eb6
johntran <tranjohn1211@gmail.com>
2023-04-03 22:08:15 -0700

    day 1 assignment

---
+++ day1/app.js
@@ -0,0 +1,7 @@
+// Project Title: Simple Calculator
+// Project Description: Create a simple calculator that performs basic arithmetic operations
+// (addition, subtraction, multiplication, division) on two numbers.
+// Project Requirements:
+// • Use variables to store the two input numbers and the result of the calculation.
+// • Use the prompt() method to get the two input numbers from the user.
+// • Use console.log() method to display the result of the calculation to the user.
