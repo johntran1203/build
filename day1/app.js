4121dc7538eb8c46f5bcb008a2fbc4340b5e33f5
johntran <tranjohn1211@gmail.com>
2023-04-03 22:20:02 -0700

    complete day1 assignment

--- day1/app.js
+++ day1/app.js
@@ -5,3 +5,12 @@
 // • Use variables to store the two input numbers and the result of the calculation.
 // • Use the prompt() method to get the two input numbers from the user.
 // • Use console.log() method to display the result of the calculation to the user.
+
+const numOne =  parseInt(prompt("what is your first number?"))
+const numTwo =  parseInt(prompt("what is your second number?"))
+
+console.log(numOne, numTwo)
+console.log( numOne + numTwo)
+console.log( numOne - numTwo)
+console.log( numOne * numTwo)
+console.log( numOne / numTwo)
