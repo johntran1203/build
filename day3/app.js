946aedbde04feb47d9ccb67a274e74a51ee4abb1
johntran <tranjohn1211@gmail.com>
2023-04-05 22:03:22 -0700

    day3 starter code assignment

---
+++ day3/app.js
@@ -0,0 +1,16 @@
+// Project Title: Simple Calculator
+// Project Description: Create a simple calculator that performs basic arithmetic operations
+// (addition, subtraction, multiplication, division) on two numbers.
+// Project Requirements:
+// • Use variables to store the two input numbers and the result of the calculation.
+// • Use the prompt() method to get the two input numbers from the user.
+// • Use console.log() method to display the result of the calculation to the user.
+
+const numOne =  parseInt(prompt("what is your first number?"))
+const numTwo =  parseInt(prompt("what is your second number?"))
+
+console.log(numOne, numTwo)
+console.log( numOne + numTwo)
+console.log( numOne - numTwo)
+console.log( numOne * numTwo)
+console.log( numOne / numTwo)
