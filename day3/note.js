13e9ac13f98a5ea577f25656008e5a6f9479fa6a
johntran <tranjohn1211@gmail.com>
2023-04-05 23:06:21 -0700

    indexOf

--- day3/note.js
+++ day3/note.js
@@ -9,3 +9,8 @@ console.log( "Hello".toUpperCase() ); // HELLO
 //An important property built-in to strings is length. We can easily look up how many characters are stored in a string by accessing this property:
 console.log( "a".length ); // 1
 console.log( "Hello".length ); // 5
+
+// We learned how to look up a character by index. Now it's time to find the index of a specific string!
+
+// There is a method on strings called indexOf that will help us find the first index of a string. Let's see this in action:
+"Hello".indexOf("e"); // 1
