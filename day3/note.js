0e43608faa338045a9106b98eaa4e18552c8c03d
johntran <tranjohn1211@gmail.com>
2023-04-05 23:01:25 -0700

    notes for charAt capitalize and length property

---
+++ day3/note.js
@@ -0,0 +1,11 @@
+//charat is to look up a character
+"Hello".charAt(1); // e
+console.log('hello'.charAt(0))
+
+// theres toLowerCase() and toUpperCase() if you want to capitalize something or not
+console.log( "Hello".toLowerCase() );// hello
+console.log( "Hello".toUpperCase() ); // HELLO
+
+//An important property built-in to strings is length. We can easily look up how many characters are stored in a string by accessing this property:
+console.log( "a".length ); // 1
+console.log( "Hello".length ); // 5
