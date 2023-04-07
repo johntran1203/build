fb8fa8528be27881be8001f0782becb3138fae96
johntran <tranjohn1211@gmail.com>
2023-04-07 02:32:20 -0700

    finish day4 assignment

--- script.js
+++ script.js
@@ -2,83 +2,89 @@
 
 // Define the todo item object
 function TodoItem(title, description, dueDate, priority, completed) {
-  this.title = title;
-  this.description = description;
-  this.dueDate = dueDate;
-  this.priority = priority;
-  this.completed = completed;
-}
-
-// Define the todo list object
-const todoList = {
-  items: [],
-
-  // Add a todo item to the list
-  addItem: function (item) {
-    if (!(item instanceof TodoItem)) {
-      throw new Error("Invalid todo item object.");
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.completed = completed;
+    
     }
-
-    //complete
-    return this.items.push(item)
-  },
-
-  // Remove a todo item from the list
-  removeItem: function (item) {
-    //complete
-  },
-
-  // Find a todo item in the list by title
-  findItemByTitle: function (title) {
-    return this.items.find((item) => item.title === title);
-  },
-
-  // Find all todo items in the list that match a given priority
-  findItemsByPriority: function (priority) {
-    //complete
-  },
-
-  // Find all todo items in the list that are due on or before a given date
-  findItemsDueBefore: function (date) {
-    //complete
-  },
-
-  // Mark a todo item as completed
-  completeItem: function (item) {
-    if (!(item instanceof TodoItem)) {
-      throw new Error("Invalid todo item object.");
-    }
-
-    //complete
-  },
-};
-
-// Test the todo list
-const item1 = new TodoItem(
-  "Buy groceries",
-  "Milk, bread, eggs",
-  new Date("2023-04-12"),
-  "High",
-  false
-);
-//create two more items
-const item2 = new TodoItem(
-  "make dinner",
-  "rice, chicken, carrots",
-  new Date("2023-04-07"),
-  "High",
-  true
-);
-
-const item3 = new TodoItem(
-  "trash",
-  "take out trash",
-  new Date("2023-04-06"),
-  "Low",
-  false
-);
-todoList.addItem(item1);
-//add 2 more items
+  
+  const todoList = {
+    items: [],
+  
+    // Add a todo item to the list
+    addItem: function (item) {
+      if (!(item instanceof TodoItem)) {
+        throw new Error("Invalid todo item object.");
+      }
+  
+      //complete
+      return this.items.push(item)
+    },
+  
+    // Remove a todo item from the list
+    removeItem: function (item) {
+      //complete
+      return this.items.pop()
+    },
+  
+    // Find a todo item in the list by title
+    findItemByTitle: function (title) {
+      return this.items.find((item) => item.title === title);
+    },
+  
+    // Find all todo items in the list that match a given priority
+    findItemsByPriority: function (priority) {
+      return this.items.filter((item) => item.priority === "High");
+      //complete
+    },
+  
+    // Find all todo items in the list that are due on or before a given date
+    findItemsDueBefore: function (date) {
+      //complete
+      return this.items.filter((item) => item.dueDate <= date);
+    },
+  
+    // Mark a todo item as completed
+    completeItem: function (item) {
+      if (!(item instanceof TodoItem)) {
+        throw new Error("Invalid todo item object.");
+      }
+      //complete
+      return item.completed = true
+  
+    },
+  }
+  const item1 = new TodoItem(
+    "Buy groceries",
+    "Milk, bread, eggs",
+    new Date("2023-04-12"),
+    "High",
+    false
+  );
+  //create two more items
+  const item2 = new TodoItem(
+    "make dinner",
+    "rice, chicken, carrots",
+    new Date("2023-04-07"),
+    "High",
+    true
+  );
+  
+  const item3 = new TodoItem(
+    "trash",
+    "take out trash",
+    new Date("2023-04-06"),
+    "Low",
+    false
+  );
+  
+  todoList.addItem(item1);
+  todoList.addItem(item2);
+  todoList.addItem(item3);
+  
+  
 
 console.log(todoList.items); // Output: [TodoItem, TodoItem, TodoItem]
 
