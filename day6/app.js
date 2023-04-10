a72d243af4d9fadf418886cb338edf878456a9d8
johntran <tranjohn1211@gmail.com>
2023-04-09 21:28:46 -0700

    day6 assignment

---
+++ day6/app.js
@@ -0,0 +1,12 @@
+// The goal of this project is to write a function that reverses the order of the array
+
+// Step 1: Define the function
+// Create a function called reverseArray that takes in an array as a parameter and returns a new
+// array with the same elements in reverse order.
+// Step 2: Test the function
+// Test the reverseArray function with some sample arrays to see if it works correctly.
+// Step 3: Implement the function as a method on the Array object
+// You can also implement the reverseArray function as a method on the Array object so that you
+// can use it directly on any array.
+// Step 4: Test the method
+// Test the reverseArray method by calling it on some sample arrays.
