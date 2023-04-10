a584ecc0f3115c48e6a74e272905a9e290f70840
johntran <tranjohn1211@gmail.com>
2023-04-09 22:14:09 -0700

    day6 done

--- day6/app.js
+++ day6/app.js
@@ -3,6 +3,7 @@
 // Step 1: Define the function
 // Create a function called reverseArray that takes in an array as a parameter and returns a new
 // array with the same elements in reverse order.
+let arr1= [1,2,3,4,5,6]
 function reverseArray(arr) {
     let result = []
     for (let i= arr.length-1; i >=0; i--) {
@@ -12,17 +13,19 @@ function reverseArray(arr) {
 }
 // Step 2: Test the function
 // Test the reverseArray function with some sample arrays to see if it works correctly.
-console.log(1,2,3,4)
+console.log(reverseArray(arr1))
 
 // Step 3: Implement the function as a method on the Array object
  Array.prototype.reverseArray = function() {
     let result = []
-    for (let i= arr.length-1; i >=0; i--) {
-        result.push(arr[i])
+    for (let i= this.length-1; i >=0; i--) {
+        result.push(this[i])
     }
     return result
 }
 // You can also implement the reverseArray function as a method on the Array object so that you
 // can use it directly on any array.
 // Step 4: Test the method
+
 // Test the reverseArray method by calling it on some sample arrays.
+console.log(arr1.reverseArray());
