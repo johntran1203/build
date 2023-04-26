4b39aee1902ba89bd42131921e54be16f4a5774f
johntran <tranjohn1211@gmail.com>
2023-04-25 19:33:24 -0700

    second question

--- day8/codewar.js
+++ day8/codewar.js
@@ -11,3 +11,21 @@ function numberToString(num) {
     // Return a string of the number here!
     return num.toString()
   }
+
+
+
+  // turn any number positive
+  //Math.abs(x)
+//   In this Kata we are passing a number (n) into a function.
+
+// Your code will determine if the number passed is even (or not).
+
+// The function needs to return either a true or false.
+
+// Numbers may be positive or negative, integers or floats.
+
+// Floats with decimal part non equal to zero are considered UNeven for this kata.
+
+function testEven(n) {
+    //Your awesome code here!
+}
